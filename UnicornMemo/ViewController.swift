0003b86d7858e735e9e5075e9fdd37c50147ee0c
Eddie <eahnivy8@gmail.com>
2020-04-27 14:03:59 +0900

    Initial Commit

---
+++ UnicornMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnicornMemo
+//
+//  Created by Eddie Ahn on 2020/04/27.
+//  Copyright © 2020 Sang Wook Ahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
